c15bd60b07d52c2307557d74864cb83e588775a5
Umoh Nsikakabasi E <91345422+NsikakabasiUmoh@users.noreply.github.com>
2022-09-11 16:06:17 -0400

    Initial commit

---
+++ Code/hangman.py
@@ -0,0 +1,6 @@
+# Import dependencies
+import random as rand
+
+# Choosing the word
+words = ['wood', 'recruit', 'bathtub', 'holiday', 'open', 'biscuit', 'exaggerate', 'light', 'overwhelm',
+         'conclusion', 'tree', 'bay']
