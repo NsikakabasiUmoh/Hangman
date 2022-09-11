e01a76bf90f73656e17853c4c1df2cad3019a6df
Umoh Nsikakabasi E <91345422+NsikakabasiUmoh@users.noreply.github.com>
2022-09-11 16:06:01 -0400

    Test completed

--- Tests/skeleton game.py
+++ Tests/skeleton game.py
@@ -2,12 +2,19 @@ chosen_word = 'wood'
 
 guess = input('Guess an alphabet: ')  # problem that can happen, two alphabets or a digit inputted
 
+# Validation
 if len(guess) > 1 or guess.isdigit():  # this will solve the above issues
     exit()
 
 if guess not in chosen_word:
     exit()
 
+# Assigning & Changing
 guess_index = [index for index, alphabet in enumerate(chosen_word) if alphabet is guess]
 
-print(f'Continue. Index: {guess_index}')
+placeholder = list(len(chosen_word)*'-')  # Placeholder variable
+
+for i in guess_index:
+    placeholder[i] = guess
+
+print(*placeholder, sep=', ')
